9972a8171e639b912b1b2095be5e9ea7678c450a
Tony612 <h.bing612@gmail.com>
2012-10-29 21:03:48 +0800

    Use gravatar to show user's photo

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,8 @@
 module UsersHelper
   
+  def avatar_for(user, size = 48)
+    default_url = "/assets/avatar.png"
+    gravatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?s=#{size}&d=mm"
+    image_tag(gravatar_url, alt: user.username, class: 'gravatar')
+  end
 end
