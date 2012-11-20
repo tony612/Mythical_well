fa1a0d0a2c4e3168145c06fe2077ebd0dd93d306
Tony612 <h.bing612@gmail.com>
2012-11-20 19:35:36 +0800

    Add some limit controller and admin

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 class MessagesController < ApplicationController
+  load_and_authorize_resource
   before_filter :authenticate_user!
   def index
     @messages = current_user.messages.includes(:comment).recent.page params[:page]
