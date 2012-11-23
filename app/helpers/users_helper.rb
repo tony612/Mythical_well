5d14b8aa2988ef5d6c694cad979835cc74019846
Tony612 <h.bing612@gmail.com>
2012-11-24 00:39:50 +0800

    Add style for some sidebar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,10 @@
 module UsersHelper
-  
-  def avatar_for(user, size = 48)
+
+  def avatar_for(user, size = 36)
     default_url = "/assets/avatar.png"
-    gravatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email.downcase)}?s=#{size}&d=mm"
-    image_tag(gravatar_url, alt: user.username, class: 'gravatar')
+    email = user.blank? ? "test@test.com" : user.email.downcase
+    username = user.blank? ? "error" : user.username
+    gravatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5::hexdigest(email)}?s=#{size}&d=mm"
+    image_tag(gravatar_url, alt: username, class: 'gravatar')
   end
 end
