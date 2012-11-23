4ec0304fd9a301293233984f1fdb54f7f83cafc9
Tony612 <h.bing612@gmail.com>
2012-11-24 01:54:39 +0800

    Pretty the style

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ require 'file_size_validator'
 class Event < ActiveRecord::Base
   attr_accessible :title, :start_date, :end_date, :date_desc, :location, :content, :category, :fee, :image, :tag_tokens, :node_id
   mount_uploader :image, ImageUploader
-  
+
   has_many :comments, :dependent => :destroy
   belongs_to :user, foreign_key: 'user_id'
 
@@ -12,7 +12,7 @@ class Event < ActiveRecord::Base
 
   has_many :event_followers
   has_many :followers, :class_name => 'User', :through => :event_followers
-  
+
   belongs_to :node, foreign_key: 'node_id'
 
 
@@ -25,23 +25,14 @@ class Event < ActiveRecord::Base
   validates :image, :presence => {message: "需要上传海报"}, :file_size => { :maximum => 1.megabytes.to_i, message: "文件大小必须在1M以内"}
   validates :node_id, presence: {message: '需要选择学校'}
   scope :recent, :order => 'start_date DESC'
-  
+
   attr_reader :tag_tokens
   def tag_tokens=(tokens)
     self.tag_ids = Tag.ids_from_tokens(tokens)
   end
 
-#  def tagging(tag_names)
-#    event_tags.where('tag_id NOT IN (?)', Tag.find_all_by_name(tag_names).map(&:id)).each {|t| t.destroy}
-#    tag_names.each do |tag|
-#      unless tags.where(name: tag).exists?
-#        if Tag.where(name: tag).exists?
-#          EventTag.create(event_id: id, tag_id: Tag.find_by_name(tag).id)
-#        else
-#          t = Tag.create(name: tag)
-#          EventTag.create(event_id: id, tag_id: t.id)
-#        end
-#      end
-#    end
-#  end
+  after_destroy :delete_img
+  def delete_img
+    remove_image
+  end
 end
