0502558c1ee32fd0693008132c7829d1640132b5
Tony612 <h.bing612@gmail.com>
2012-11-24 20:16:44 +0800

    Add the rescue from cancan and fix the admin ability show

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,15 +1,15 @@
 # encoding: utf-8
 class Admin::ApplicationController < ApplicationController
-  layout 'admin' 
+  layout 'admin'
 
   before_filter :authenticate_user!
   before_filter :require_admin
 
   def require_admin
     if not Setting.admin_emails.include?(current_user.email)
-      flash[:warning] = "对不起，您没有这个权限"
+      flash[:warning] = "不好意思，您是不是点错什么地方了？"
       redirect_to root_path
     end
   end
-  
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 # encoding: utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   before_filter :make_bread
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:warning] = "不好意思哦，你没有权限这么做，如果你一定要的话，请登录正确的帐号"
+    redirect_to root_path
+
+  end
 
   private
 
